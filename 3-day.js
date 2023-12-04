94822a47c8fb4d409acb765cd8073d3e52f0601f
Olzhas Seitmagambet <sei7magambet@gmail.com>
2023-12-05 00:30:50 +0600

    3.2

--- 3-day.js
+++ 3-day.js
@@ -2,40 +2,61 @@ import { getLines } from './lib.js';
 
 const lines = getLines('./3-input.txt');
 
-const isSymbol = char => typeof char === 'string' && !/\d|\./.test(char);
+const isNumber = char => typeof char === 'string' && /\d/.test(char);
 
 let total = 0;
+
+function check(numbers, lines, visited, i, j) {
+  if (!visited.has(`${i}/${j}`) && isNumber(lines[i]?.[j])) {
+    numbers.push(extractNumber(visited, lines, i, j));
+  }
+}
+
+function findNumbers(lines, i, j, visited) {
+  const numbers = [];
+
+  check(numbers, lines, visited, i, j - 1);
+  check(numbers, lines, visited, i - 1, j - 1);
+  check(numbers, lines, visited, i - 1, j);
+  check(numbers, lines, visited, i - 1, j + 1);
+  check(numbers, lines, visited, i, j + 1);
+  check(numbers, lines, visited, i + 1, j + 1);
+  check(numbers, lines, visited, i + 1, j);
+  check(numbers, lines, visited, i + 1, j - 1);
+
+  return numbers;
+}
+
+function extractNumber(visited, lines, i, j) {
+  while (/\d/.test(lines[i][j])) {
+    j -= 1;
+  }
+
+  let num = '';
+
+  j += 1;
+
+  while (/\d/.test(lines[i][j])) {
+    visited.add(`${i}/${j}`);
+    num += lines[i][j];
+    j += 1;
+  }
+
+  return parseInt(num);
+}
 
 for (let i = 0; i < lines.length; i++) {
   const line = lines[i];
 
-  let num = '';
-  let isAdjacent = false;
-
   for (let j = 0; j < line.length; j++) {
     const char = line[j];
 
-    if (/\d/.test(char)) {
-      num += char;
+    if (char === '*') {
+      const numbers = findNumbers(lines, i, j, new Set());
 
-      isAdjacent ||=
-        isSymbol(lines[i][j - 1]) || // left
-        isSymbol(lines[i - 1]?.[j - 1]) || // left top
-        isSymbol(lines[i - 1]?.[j]) || // top
-        isSymbol(lines[i - 1]?.[j + 1]) || // top right
-        isSymbol(lines[i][j + 1]) || // right
-        isSymbol(lines[i + 1]?.[j + 1]) || // bottom right
-        isSymbol(lines[i + 1]?.[j]) || // bottom
-        isSymbol(lines[i + 1]?.[j - 1]); // bottom left
-    }
-
-    if (!/\d/.test(line[j + 1])) {
-      if (num !== '' && isAdjacent) {
-        total += parseInt(num);
+      if (numbers.length > 1) {
+        total += numbers.reduce((a, b) => a * b, 1);
       }
-
-      num = '';
-      isAdjacent = false;
     }
   }
 }
